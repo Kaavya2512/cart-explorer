1cc816576ab5a397f82d7d0f7c3fa9c90c26e07e
Kaavya Barathi <2512+Kaavya2512@users.noreply.github.com>
2025-11-10 00:02:02 +0530

    app and main ts had code changes

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   standalone: true,
+  imports: [RouterOutlet],
   templateUrl: './app.html',
-  styleUrl: './app.css'
+  styleUrls: ['./app.css']
 })
 export class AppComponent {
   title = 'cart-explorer';
